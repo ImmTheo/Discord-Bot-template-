2b20eeef059be55905dc6079f77cff7a194d70a6
Théo <90760766+ImmTheo@users.noreply.github.com>
2021-09-19 13:14:54 +0200

    Update for Discord.js v13

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const Discord = require('discord.js');
-const client = new Discord.Client();
+const client = new Discord.Client({intents: []});
 const fs = require('fs');
 const {TOKEN, PREFIX} = require("./config.js");
 
